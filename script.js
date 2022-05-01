75493e3711f0741ac04869c492b20f65b70f27cd
Cyber Sepp <sgmail1000@t-online.de>
2022-05-01 03:11:02 +0200

    complete challenge

--- script.js
+++ script.js
@@ -1,12 +1,49 @@
 const container = document.querySelector('#container')
 const slider = document.querySelector('#slider')
+const button = document.querySelector('#button')
+const button2 = document.querySelector('#button2')
 
+let miniSquareAmount = 0
+let gridSize = 8
+let squareSize = 512/gridSize
+let colorMode = false
 
- 
+//Setup event listener on "Set Grid size" button
+button.addEventListener('click', function() {
+    let gridSize = parseInt(prompt("Please enter your desired grid size."))
+    if (gridSize >= 50) {
+        gridSize = 50
+    }
+    if (gridSize < 0) {
+        gridSize = 2
+    }
+    squareSize = 512/gridSize
+    resetField();
+    spawnField(gridSize)
+}, false);
 
- let miniSquareAmount = 0
+//Setup event listener on "Color Mode" button
+button2.addEventListener('click', function() {
+    if (colorMode === false) {
+        button2.innerHTML = "Color Mode: On"
+        colorMode = true
+    }
+    else if (colorMode === true) {
+        button2.innerHTML = "Color Mode: Off"
+        colorMode = false
+    }
+}, false);
 
+//Check whether mouse button is held
+let mouseDown = 0;
+document.body.onmousedown = function() { 
+  ++mouseDown;
+}
+document.body.onmouseup = function() {
+  --mouseDown;
+}
 
+//Spawn field with desired size
 function spawnField(size) {
 for (i=1; i<size+1; i++) {
     let currentRow = document.createElement('div')
@@ -18,42 +55,57 @@ for (i=1; i<size+1; i++) {
         
         currentMiniSquare.classList.add('minisquare')
         currentMiniSquare.id = i
+        
+        //size
+        currentMiniSquare.style.height = `${squareSize}px`
+        currentMiniSquare.style.width = `${squareSize}px`
+        //console.log("Height: "+currentMiniSquare.style.height)
+        //console.log("Width: "+currentMiniSquare.style.width)
+
         miniSquareAmount++
         currentRow.appendChild(currentMiniSquare)
         //console.log(miniSquareAmount)
+        currentMiniSquare.addEventListener('mouseover', function() {
+            if (mouseDown === 1) {
+            var r = () => Math.random() * 256 >> 0;
+            var color = `rgb(${r()}, ${r()}, ${r()})`;
+            //console.log(color)
+            currentMiniSquare.classList.add('clicked')
+            if (colorMode === true) {
+            currentMiniSquare.style.backgroundColor = color
+            }
+            else if (colorMode === false) {
+                currentMiniSquare.style.backgroundColor = "white"
+                }
+            }
+        }, false);
+
     }
 }
 }
 
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
+  
+
+//Remove all child nodes from parent
 function removeAllChildNodes(parent) {
     while (parent.firstChild) {
         parent.removeChild(parent.firstChild);
     }
 }
 
-let miniSquares = document.querySelectorAll('.minisquare');
-console.log(miniSquares.length)
-miniSquares.forEach((div) => {
-
-    // and for each one we add a 'click' listener
-    div.addEventListener('click', () => {
-      alert(button.id);
-    });
-  });
-
-spawnField(16)
-/* function resetField () {
+//Reset the field
+function resetField () {
     removeAllChildNodes(container);
-} */
+}
 
-
-/* slider.addEventListener ("input", function () {
-    //spawnField(this.value)
-    resetField()
-    spawnField(this.value)
-    console.log(this.value)
- }); */
-
-
-
+//Spawn a 16x16 field
+spawnField(gridSize)
 
